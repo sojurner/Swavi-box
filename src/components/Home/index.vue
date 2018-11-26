1f21c47cc84b2e3773ca58861a1c8f448cb7acf6
sojurner <pykim0591@gmail.com>
2018-11-25 19:05:28 -0700

    Create Home component

---
+++ src/components/Home/index.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="home-page">
+  <p>Swavi-Box</p>
+  <BackgroundScroll></BackgroundScroll>
+  <router-view></router-view>
+  <i @click="toggleNav" :class="icon_class"></i>
+  <NavBar v-if="displayNav"></NavBar>
+</div>
+</template>
+
+<script>
+import NavBar from '../NavBar';
+import BackgroundScroll from '../BackgroundScroll';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      icon_class: 'fas fa-stream',
+      displayNav: false
+    };
+  },
+
+  components: {
+    NavBar,
+    BackgroundScroll
+  },
+
+  methods: {
+    toggleNav() {
+      this.displayNav = !this.displayNav;
+      if (this.displayNav) {
+        this.icon_class = 'far fa-times-circle';
+      } else {
+        this.icon_class = 'fas fa-stream';
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.home-page {
+  display: grid;
+  grid-template-columns: 10rem 1fr;
+  grid-template-rows: 1fr 3fr 1fr;
+}
+</style>
