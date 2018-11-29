be1ac6ee27e51edf1cb5288a768efc586aee0c75
sojurner <pykim0591@gmail.com>
2018-11-28 20:13:03 -0700

    Add webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  }
+};
